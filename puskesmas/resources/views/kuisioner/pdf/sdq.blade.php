8d66016fb7a4a30528ee6deeec92a61d870ef1e9
AnoNe17 <atomx361@gmail.com>
2023-07-10 02:56:45 +0700

    update pdf ketearngan sdq

--- puskesmas/resources/views/kuisioner/pdf/sdq.blade.php
+++ puskesmas/resources/views/kuisioner/pdf/sdq.blade.php
@@ -10,6 +10,9 @@
 		table tr th{
 			font-size: 9pt;
 		}
+		table tr th{
+			width: 25%
+		}
 	</style>
 	<center>
 		<h4>Puskesmas Kertasemaya</h4>
@@ -18,73 +21,184 @@
 		<h5>{{ $data[0]->nama }} ({{ $data[0]->instansi }})</h5>
         <br>
 	</center>
- 
+
+    <center>
+        <h4>Skor keseluruhan</h4>
+        <h1>{{ $data[0]->skor_keseluruhan }}</h1>
+        <br>
+    </center>
+
+    {{-- Gejala Emosional --}}
+    <h6>Gejala Emmosional</h6>
 	<table class='table table-bordered'>
-		<thead>
-			<tr>
-                {{-- <th>Hasil Kesulitan</th> --}}
-                <th>Gejala Emosional</th>
-                <th>Gejala Perilaku</th>
-                <th>Hiperaktivitas</th>
-                <th>Masalah Teman Sebaya</th>
-                <th>Perilaku Proposional</th>
-                <th>Dibuat Pada</th>
-            </tr>
-		</thead>
-		<tbody>
-			<tr>
-				{{-- <td>{{$data[0]->hasil_kesulitan}}</td> --}}
-				<td>
-                    @if ($data[0]->hasil_e === 'NORMAL')
-                        <div class="text-success">{{ $data[0]->hasil_e }}</div>
-                    @elseif($data[0]->hasil_e === 'BORDERLINE / AMBANG')
-                        <div class="text-warning">{{ $data[0]->hasil_e }}</div>
-                    @else
-                        <div class="text-danger">{{ $data[0]->hasil_e }}</div>
-                    @endif
-                </td>
-				<td>
-                    @if ($data[0]->hasil_c === 'NORMAL')
-                        <div class="text-success">{{ $data[0]->hasil_c }}</div>
-                    @elseif($data[0]->hasil_c === 'BORDERLINE / AMBANG')
-                        <div class="text-warning">{{ $data[0]->hasil_c }}</div>
-                    @else
-                        <div class="text-danger">{{ $data[0]->hasil_c }}</div>
-                    @endif
-                </td>
-				<td>
-                    @if ($data[0]->hasil_h === 'NORMAL')
-                        <div class="text-success">{{ $data[0]->hasil_h }}</div>
-                    @elseif($data[0]->hasil_h === 'BORDERLINE / AMBANG')
-                        <div class="text-warning">{{ $data[0]->hasil_h }}</div>
-                    @else
-                        <div class="text-danger">{{ $data[0]->hasil_h }}</div>
-                    @endif
-                </td>
-				<td>
-                    @if ($data[0]->hasil_p === 'NORMAL')
-                        <div class="text-success">{{ $data[0]->hasil_p }}</div>
-                    @elseif($data[0]->hasil_p === 'BORDERLINE / AMBANG')
-                        <div class="text-warning">{{ $data[0]->hasil_p }}</div>
-                    @else
-                        <div class="text-danger">{{ $data[0]->hasil_p }}</div>
-                    @endif
-                </td>
-				<td>
-                    @if ($data[0]->hasil_pro === 'NORMAL')
-                        <div class="text-success">{{ $data[0]->hasil_pro }}</div>
-                    @elseif($data[0]->hasil_pro === 'BORDERLINE / AMBANG')
-                        <div class="text-warning">{{ $data[0]->hasil_pro }}</div>
-                    @else
-                        <div class="text-danger">{{ $data[0]->hasil_pro }}</div>
-                    @endif
-                </td>
-				<td>
-                    {{ $data[0]->created_at }}
-                </td>
-			</tr>
-		</tbody>
+        <tr>
+            <th>Skor</th>
+            <td>
+                @if ($data[0]->hasil_e === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->skor_e }}</div>
+                @elseif($data[0]->hasil_e === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->skor_e }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->skor_e }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Hasil</th>
+            <td>
+                @if ($data[0]->hasil_e === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->hasil_e }}</div>
+                @elseif($data[0]->hasil_e === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->hasil_e }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->hasil_e }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Keterangan</th>
+            <td>
+                {!! $data[0]->keterangan_e !!}
+            </td>
+        </tr>
 	</table>
+
+    <h6>Gejala Perilaku</h6>
+	<table class='table table-bordered'>
+        <tr>
+            <th>Skor</th>
+            <td>
+                @if ($data[0]->hasil_c === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->skor_c }}</div>
+                @elseif($data[0]->hasil_e === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->skor_c }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->skor_c }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Hasil</th>
+            <td>
+                @if ($data[0]->hasil_c === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->hasil_c }}</div>
+                @elseif($data[0]->hasil_e === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->hasil_c }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->hasil_c }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Keterangan</th>
+            <td>
+                {!! $data[0]->keterangan_c !!}
+            </td>
+        </tr>
+	</table>
+
+    <h6>Hiperaktivitas</h6>
+	<table class='table table-bordered'>
+        <tr>
+            <th>Skor</th>
+            <td>
+                @if ($data[0]->hasil_h === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->skor_h }}</div>
+                @elseif($data[0]->hasil_h === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->skor_h }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->skor_h }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Hasil</th>
+            <td>
+                @if ($data[0]->hasil_h === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->hasil_h }}</div>
+                @elseif($data[0]->hasil_h === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->hasil_h }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->hasil_h }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Keterangan</th>
+            <td>
+                {!! $data[0]->keterangan_h !!}
+            </td>
+        </tr>
+	</table>
+
+    <h6>Masalah Teman Sebaya</h6>
+	<table class='table table-bordered'>
+        <tr>
+            <th>Skor</th>
+            <td>
+                @if ($data[0]->hasil_p === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->skor_p }}</div>
+                @elseif($data[0]->hasil_p === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->skor_p }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->skor_p }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Hasil</th>
+            <td>
+                @if ($data[0]->hasil_h === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->hasil_p }}</div>
+                @elseif($data[0]->hasil_h === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->hasil_p }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->hasil_p }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Keterangan</th>
+            <td>
+                {!! $data[0]->keterangan_p !!}
+            </td>
+        </tr>
+	</table>
+
+    <h6>Perilaku Proposional</h6>
+	<table class='table table-bordered'>
+        <tr>
+            <th>Skor</th>
+            <td>
+                @if ($data[0]->hasil_pro === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->skor_pro }}</div>
+                @elseif($data[0]->hasil_pro === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->skor_pro }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->skor_pro }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Hasil</th>
+            <td>
+                @if ($data[0]->hasil_pro === 'NORMAL')
+                    <div class="text-success">{{ $data[0]->hasil_pro }}</div>
+                @elseif($data[0]->hasil_pro === 'BORDERLINE / AMBANG')
+                    <div class="text-warning">{{ $data[0]->hasil_pro }}</div>
+                @else
+                    <div class="text-danger">{{ $data[0]->hasil_pro }}</div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>Keterangan</th>
+            <td>
+                {!! $data[0]->keterangan_pro !!}
+            </td>
+        </tr>
+	</table>
+
     <br><br><br><br>
     @if (isset($code_verif))
         <center>
